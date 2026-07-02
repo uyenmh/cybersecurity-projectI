3154b1c576fee8325c973b4d1fcae4ea6302b5d8
Uyen <152192561+uyenmh@users.noreply.github.com>
2026-07-02 05:46:44 +0300

    create the entry model

--- journals/models.py
+++ journals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Entry(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
